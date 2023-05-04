31444af37887ca603082a02e9d2e8cf7f2c9a70a
compudiegopage <compu.diego94@gmail.com>
2023-05-04 01:32:23 -0300

    form contacto funcionando completo

--- js/funciones.js
+++ js/funciones.js
@@ -2,3 +2,30 @@ document.querySelector('.first-button').addEventListener('click', function () {
 
   document.querySelector('.animated-icon1').classList.toggle('open');
   });
+
+  $(function(){
+
+    $('a[href*=#]').click(function() {
+
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+
+            var $target = $(this.hash);
+
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+            if ($target.length) {
+
+                var targetOffset = $target.offset().top;
+
+                $('html,body').animate({scrollTop: targetOffset}, 500);
+
+                return false;
+
+           }
+
+      }
+
+  });
+
+});
